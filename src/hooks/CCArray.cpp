3a161a6e9722673150690f8104983cd70e643093
SabeMP <sabemasterprox@gmail.com>
2024-04-01 02:04:12 -0300

    Fix weird indentation

--- src/hooks/CCArray.cpp
+++ src/hooks/CCArray.cpp
@@ -6,7 +6,7 @@ using namespace geode::prelude;
 unsigned int PracticePlusCCArray::count() {
     unsigned int l_count = CCArray::count();
     PracticePlusPlayLayer* l_playLayer = static_cast<PracticePlusPlayLayer*>(PlayLayer::get());
-	if (l_playLayer && this == l_playLayer->m_checkpointArray) {
+    if (l_playLayer && this == l_playLayer->m_checkpointArray) {
         if (l_playLayer->m_fields->m_enableArrayHook &&
             l_playLayer->isPlusMode() &&
             l_playLayer->getActiveStartpointId() != -1
@@ -20,7 +20,7 @@ unsigned int PracticePlusCCArray::count() {
 CCObject* PracticePlusCCArray::lastObject() {
     CCObject* l_lastObject = CCArray::lastObject();
     PracticePlusPlayLayer* l_playLayer = static_cast<PracticePlusPlayLayer*>(PlayLayer::get());
-	if (l_playLayer && this == l_playLayer->m_checkpointArray && !l_lastObject) {
+    if (l_playLayer && this == l_playLayer->m_checkpointArray && !l_lastObject) {
         if (l_playLayer->m_fields->m_enableArrayHook &&
             l_playLayer->isPlusMode() &&
             l_playLayer->getActiveStartpointId() != -1
